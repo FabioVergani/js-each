6b5c66ff44161f352d52196c2f80c13355269c35
Fabio Vergani <fabio.vergani.1975@gmail.com>
2016-07-12 18:43:30 +0200

    Create each.js

---
+++ each.js
@@ -0,0 +1,9 @@
+Array.prototype.each=function(x){
+	var f=x,i,m,l;
+	if(f){i=0;m=this;l=m.length;while(i<l){f(m[i],i);++i;};};
+};
+
+/*
+var m=[1,2,3,4,5];
+m.each(function(value,index){console.log(index+'/'+value);});
+*/
