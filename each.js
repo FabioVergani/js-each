8247d519dea7ca5e2518f4bcd11fad75101a60c1
Fabio Vergani <fabio.vergani.1975@gmail.com>
2016-07-12 18:46:50 +0200

    Update each.js

--- each.js
+++ each.js
@@ -1,9 +1,11 @@
 Array.prototype.each=function(x){
 	var f=x,i,m,l;
-	if(f){i=0;m=this;l=m.length;while(i<l){f(m[i],i);++i;};};
+	if(f){i=0;m=this;l=m.length;while(i<l){f(m[i],i,m);++i;};};
 };
+
+
 
 /*
 var m=[1,2,3,4,5];
-m.each(function(value,index){console.log(index+'/'+value);});
+m.each(function(value,index,thearray){console.log(index+'/'+value,thearray);});
 */
